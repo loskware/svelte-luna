87fe1568f3031a660ec6c87668aaa133810b9eb4
Aernom <andrea.montorio@gmail.com>
2022-02-21 09:29:46 +0100

    removed ./svelte-kit from tracking

--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,6 +0,0 @@
-import * as module from '../entries/pages/layout.svelte.js';
-
-export { module };
-export const entry = 'layout.svelte-ef2b53d3.js';
-export const js = ["layout.svelte-ef2b53d3.js","chunks/vendor-7df82684.js"];
-export const css = [];
--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,6 +0,0 @@
-import * as module from '../entries/pages/error.svelte.js';
-
-export { module };
-export const entry = 'error.svelte-86921a23.js';
-export const js = ["error.svelte-86921a23.js","chunks/vendor-7df82684.js"];
-export const css = [];

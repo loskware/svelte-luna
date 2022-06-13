7ff82d9f33bba47678dcec1fd3e9be9afdaacfa6
Aernom <andrea.montorio@gmail.com>
2022-06-13 14:59:52 +0200

    removed debug code

--- src/lib/actions/portal.ts
+++ src/lib/actions/portal.ts
@@ -1,10 +1,7 @@
-let index = 0
-
 /**
  * Append node to the target element
  */
 export function portal(node: HTMLElement, target: HTMLElement = document.body) {
-  node.setAttribute("data-index", String(index++));
   target.appendChild(node);
   return {
     destroy() { node.remove() }
